6f4f1ef8e117d2fe69b356cc094e4f93dd69eeb5
vd <mrafighalibf@gmail.com>
2026-02-20 00:33:28 +0700

    chore: initialize project

---
+++ src/main/java/id/ac/ui/cs/advprog/yomuachievement/YomuAchievementApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomuachievement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YomuAchievementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YomuAchievementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/yomuachievement/YomuAchievementApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.yomuachievement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YomuAchievementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
